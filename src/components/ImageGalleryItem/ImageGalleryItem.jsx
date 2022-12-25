c6b6114ea61a584004b36c7d3a9755afcf582e82
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-12-25 22:35:52 +0200

    fix

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,9 @@
 import PropTypes from 'prop-types';
 import { ImageItem, Image } from './ImageGalleryItem.styled';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export const ImageGelleryItem = ({ webformatURL, id, tags, toggleModal }) => {
   return (
     <ImageItem onClick={() => toggleModal(id)}>
-      {/* <LazyLoadImage
-        alt={tags}
-        src={webformatURL} // use normal <img> attributes as props
-        height={'260px'}
-        effect="blur"
-      /> */}
       <Image src={webformatURL} alt={tags} />
     </ImageItem>
   );
