65f19b73c186379c8e5d872d8467dcc4496d1c27
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-12-25 22:34:00 +0200

    change size of images

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const ImagesList = styled.ul`
   display: grid;
+  width: 1600px;
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
